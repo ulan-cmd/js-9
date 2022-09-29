b562440862af9c0939107349081db2ad8d3e660b
ulancho <ulan.five@gmail.com>
2022-09-29 21:32:49 +0600

    feat: 29-09

---
+++ 29-09/script.js
@@ -0,0 +1,85 @@
+/*
+let name1 = 'Ulan';
+let name2 = 'Jumabaev';
+name2 = name1;
+alert(name2);
+*/
+
+/******** Число ********/
+/*let age = 50;
+let price = 50.5;
+alert(age);
+alert(price);*/
+
+//infinity
+/*
+const result = 20 / 0;
+alert(result);
+*/
+
+//NaN - not a number
+/*const a = 15;
+const b = 'hello';
+const result = a * b;
+alert(result);*/
+
+
+/******** Строка ********/
+/*const name = 'Ulan';
+const login = "ulan@gmail.com";
+const token = `${name} 1234568test ${login}`;
+alert(token);*/
+
+
+/******** Логический ********/
+/*const isProgrammer = true;
+const isAdmin = false;
+const result = 20 === 15;
+alert(result);*/
+
+
+/******** Null ********/
+// let user_name = null;
+
+
+/******** Undefined ********/
+// let password;
+// let url;
+// alert(url);
+
+
+/******** Объект ********/
+/*let user = {
+    name:'Ulan',
+    surname:'Jumabaev',
+    isProgrammer:true
+}
+
+console.log('Teeeeest');
+console.log(123);*/
+
+/******** Bigint ********/
+const num1 = 1234567890123456789n;
+console.log(num1);
+
+
+/******** Typeof ********/
+let user = {
+    name:'Ulan',
+    surname:'Jumabaev',
+    isProgrammer:true
+}
+
+console.log(typeof user);
+
+
+
+
+
+
+
+
+
+
+
+
