0aeaf126302122dced140d29257b1059dad9c2a4
ulancho <ulan.five@gmail.com>
2022-10-01 21:31:37 +0600

    feat: 01 - 10

---
+++ 01-10/script.js
@@ -0,0 +1,134 @@
+/******* математические операции *******/
+/*
+const num1 = 5;
+const num2 = 10;
+const result1 = num1 + num2;
+console.log(result1);
+
+const a = 10;
+const b = 1;
+const result2 = a - b;
+console.log(result2);
+
+const result3 = 10 * 100;
+console.log(result3);
+
+const x = 75;
+const result4 = x / 10;
+console.log(result4);*/
+
+
+/*
+const salary = 45;
+const result = salary % 10;
+console.log(result); //5
+*/
+
+
+/******* постфиксный инкремент *******/
+/*let age = 45;
+let a = age++;
+console.log(a);   //45
+console.log(age); //46
+
+let count = 5;
+count++;
+console.log(count); //6*/
+
+/******* префиксный инкремент *******/
+/*let x = 63;
+let result = ++x;
+console.log(x);      //64
+console.log(result); //64
+
+let products_count = 100;
+++products_count;
+console.log(products_count); //101*/
+
+
+/******* постфиксный декремент *******/
+/*let count = 45;
+let result = count--;
+console.log(count);   //44
+console.log(result);  //45
+
+let d = 36;
+d--;
+console.log(d); //35*/
+
+/******* префиксный декремент *******/
+/*
+let m = 10;
+let x = --m;
+console.log(m); //9
+console.log(x); //9
+
+let price = 75;
+--price;
+console.log(price); //74
+*/
+
+
+/******* += *******/
+/*let x = 50;
+x += 50; //x = x + 50
+console.log(x); //100*/
+
+/******* -= *******/
+/*let age = 20;
+age -= 10; //age = age - 10;
+console.log(age); // 10*/
+
+/******* *= *******/
+/*let a = 5;
+let c = 4;
+a *= c; //a = a * c
+console.log(a); //20*/
+
+/******* /= *******/
+/*let num1 = 100;
+let num2 = 10;
+num1 /= num2; // num1 = num1 / num2;
+console.log(num1); //10*/
+
+/******* == *******/
+/*const str1 = 'test';
+const str2 = 'test';
+const result = str1 === str2;
+console.log(result); //true
+
+const age = 20;
+const result2 = age === 10;
+console.log(result2); //false*/
+
+
+/******* === *******/
+/*const age1 = 25;
+const age2 = '25';
+console.log(typeof age1);
+console.log(typeof age2);
+console.log(age1 === age2); // false
+console.log(age1 + parseInt(age2));*/
+
+/******* != *******/
+const x = 'Bishkek';
+const result = x !== 'Bishkek'; //false
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
